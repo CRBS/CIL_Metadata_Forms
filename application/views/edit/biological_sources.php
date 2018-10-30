7a0ae37ce8d7cafc0fde9433b9b404a663c753a7
Willy Wong <wawong@gmail.com>
2018-10-30 15:28:53 -0700

    Working on the description and details

---
+++ application/views/edit/biological_sources.php
@@ -0,0 +1,30 @@
+<br/>
+<span class="cil_title2">Biological Sources</span>
+<div class="row">
+    <div class="col-md-6">
+        <div class="form-group">
+            <label class="col-form-label" for="inputDefault"> NCBI Organismal Classification</label>
+            <input id="image_search_parms_ncbi" name="image_search_parms[ncbi]" style="width: 100%" value="" class="form-control cil_san_regular_font ui-autocomplete-input" autocomplete="off" type="text">
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="form-group">
+            <label class="col-form-label" for="inputDefault"> Cell Type</label>
+            <input id="image_search_parms_cell_type" name="image_search_parms[cell_type]" style="width: 100%" type="text" value="" class="form-control cil_san_regular_font ui-autocomplete-input" autocomplete="off"> 
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-6">
+        <div class="form-group">
+            <label class="col-form-label" for="inputDefault"> Cell Line</label>
+            <input id="image_search_parms_cell_line" name="image_search_parms[cell_line]" style="width: 100%" type="text" value="" class="form-control cil_san_regular_font ui-autocomplete-input" autocomplete="off">
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="form-group">
+            <label class="col-form-label" for="inputDefault"> Cell Component (GO)</label>
+            <input id="image_search_parms_cellular_component" name="image_search_parms[cellular_component]" style="width: 100%" type="text" value="" class="form-control cil_san_regular_font ui-autocomplete-input" autocomplete="off">
+        </div>
+    </div>
+</div>
---
+++ application/views/edit/desc_n_tech.php
@@ -0,0 +1,15 @@
+<br/>
+<span class="cil_title2">Description</span>
+<div class="row">
+    <div class="col-md-12">
+        <textarea id="description" name="description" rows="4" cols="50" placeholder=""></textarea> 
+    </div>
+</div>
+<br/>
+<span class="cil_title2">Technical Details</span>
+<div class="row">
+    <div class="col-md-12">
+        <textarea id="tech_details" name="tech_details" rows="4" cols="50" placeholder=""></textarea> 
+    </div>
+</div>
+
