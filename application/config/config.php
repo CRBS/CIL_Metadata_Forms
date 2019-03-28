a4e6c52284d6db64b53e2f5f26e1ba577fdc7b93
Willy Wong <wawong@gmail.com>
2019-03-27 19:40:22 -0700

    Updated the config file

--- application/config/config.php
+++ application/config/config.php
@@ -386,6 +386,7 @@ $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
 //$config['sess_save_path'] = NULL;
 $config['sess_save_path'] = 'C:/Users/wawong/Documents/apache/CIL_Metadata_Forms/Apache24/temp';
+//$config['sess_save_path'] = '/var/www/temp';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
