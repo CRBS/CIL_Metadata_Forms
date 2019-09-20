21698cceb6c4b34e47c62964d19f01807d54f5f5
Willy Wong <wawong@gmail.com>
2019-09-19 22:14:28 -0700

    Output the model json files

--- application/controllers/Rest.php
+++ application/controllers/Rest.php
@@ -9,7 +9,25 @@ class Rest extends REST_Controller
     public function model_json_get($model_id)
     {
         $dbutil = new DB_util();
+        
+        $model_upload_location = $this->config->item('model_upload_location');
+        
         $mjson = $dbutil->getModelJson($model_id);
+        
+        
+        
+        /************Outputing to a file *****************/
+        $folder = $model_upload_location."/model_json_files";
+        if(!file_exists($folder))
+            mkdir($folder);
+        
+        $filePath = $folder."/".$model_id.".json";
+        $json_str = json_encode($mjson,JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+        if(file_exists($filePath))
+            unlink($filePath);
+        
+        file_put_contents($filePath, $json_str);
+        /***********************************************/
         
         if(!is_null($mjson))
         {
@@ -21,6 +39,8 @@ class Rest extends REST_Controller
             $array['Error'] = "Cannot find the result";
             $this->response($array);   
         }
+        
+        
     }
     
      public function metadata_json_get($id)
