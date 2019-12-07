e03bdabb82a981d498c6b882fa100afbeb0808ae
Willy Wong <wawong@gmail.com>
2019-12-06 23:00:52 -0800

    Working on the web user page

--- application/views/home/member_home_display.php
+++ application/views/home/member_home_display.php
@@ -4,23 +4,31 @@
         <div class="col-md-4">
             <div class="row">
                 <div class="col-md-12">
-                    <span class="cil_title2">CDeep3M</span>
+                    <span class="cil_title2">Upload trained model</span>
                 </div>
+                
                 <div class="col-md-12">
-                    <a href="/cdeep3m_models/list_models" target="_self" class="btn btn-info"> All trained models </a>
+                    <img src="https://cildata.crbs.ucsd.edu/media/model_display/50685/50685_thumbnailx512.jpg" width="100%" />
+                </div>
+                 <div class="col-md-12"><hr style="height:1px; visibility:hidden;" /></div>
+                <div class="col-md-12">
+                    A new trained model is generated based on training images and labels. For 3D segmentation tasks CDeep3M trains three different models seeing one frame (1 fm), seeing three frames (3 fm), and seeing five frames (5 fm) that are applied to image data.
+                </div>
+                 <div class="col-md-12"><hr style="height:1px; visibility:hidden;" /></div>
+                <div class="col-md-12">
+                    
+                    <a href="/cdeep3m_models/new_model" class="btn btn-primary">Upload CDeep3M Model</a>
                 </div>
             </div>
         </div>
         <div class="col-md-4">
-            <div class="col-md-12">
-                <img src="https://cildata.crbs.ucsd.edu/media/model_display/50685/50685_thumbnailx512.jpg" />
-            </div>
+            <!--
             <div class="col-md-12">
                     <a href="/cdeep3m_models/new_model" class="btn btn-primary">Upload CDeep3M Model</a>
             </div>
             <div class="col-md-12">
                     <a href="/cdeep3m_models/new_model" class="btn btn-primary">Upload CDeep3M Model</a>
-            </div>
+            </div> -->
         </div>
         <div class="col-md-4"></div>
     </div>
