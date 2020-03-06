df91297deea6010febe123265e90593d31161d2d
Willy Wong <wawong@gmail.com>
2020-03-05 16:45:02 -0800

    Added the prp logo

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -4,27 +4,32 @@
     <div class="row footer_background">
         <div class="col-md-12 "><br/></div>
         
-        <div class="col-md-4 pull-right">
+        <div class="col-md-3 pull-right">
     <!-- <a href="http://crbs.ucsd.edu/" class="grid_3 suffix_1" target="_blank"><img alt="crbs" src="/pix/CRBS_LOGO.png"></a> -->
-            &nbsp;
+          <!--  &nbsp; -->
+          
         </div>
-    
-        <div class="col-md-8 pull-right">
-            <address>
-            <strong>Center for Research in Biological Systems</strong><br>
-            Basic Science Building, Room 1000 <br>
-            University of California, San Diego<br>
-            9500 Gilman Drive<br>
-            La Jolla, CA 92093-0608, USA<br>
-            
-            
-            <strong>Voice</strong>: (858) 534-0276<br>
-            <strong>Fax</strong>: (858) 534-7497<br>
-            <strong>Email</strong>: cdeep3m@ucsd.edu
-            
-        </address>
+        <div class="col-md-2">
+            <a href="https://pacificresearchplatform.org/" target="_blank"><img src="/pix/prp_logo.png" /></a>
         </div>
-        
+        <div class="col-md-6">
+            
+                    <address>
+                    <a href="http://crbs.ucsd.edu/" target="_blank"><strong>Center for Research in Biological Systems</strong><br></a>
+                    Basic Science Building, Room 1000 <br>
+                    University of California, San Diego<br>
+                    9500 Gilman Drive<br>
+                    La Jolla, CA 92093-0608, USA<br>
+
+
+                    <strong>Voice</strong>: (858) 534-0276<br>
+                    <strong>Fax</strong>: (858) 534-7497<br>
+                    <strong>Email</strong>: cdeep3m@ucsd.edu
+                    <br/><br/>
+                    </address> 
+              
+        </div>
+        <div class="col-md-1"></div>
     </div>
     </center>
     
