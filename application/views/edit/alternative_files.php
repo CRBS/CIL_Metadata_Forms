ef35b1cdf44808a3331381ef02178286ca238bdd
Willy at NCMIR <wawong@gmail.com>
2023-09-27 15:57:12 -0700

    Added alternative_files.php

---
+++ application/views/edit/alternative_files.php
@@ -0,0 +1,51 @@
+<br/>
+<span class="cil_title2">Alternative files</span>
+<div class="row">
+    <div class='col-md-6'>
+        <label class="col-form-label" for="inputDefault">Mime type</label><br/>
+        <select name="Mime_type" id="Mime_type" class='form-control cil_san_regular_font'>
+            <option value="application/octet-stream">application/octet-stream</option>
+            <option value="application/zip">application/zip</option>
+        </select>
+    </div>
+    <div class='col-md-6'></div>
+</div>
+
+<div class="row">
+    <div class='col-md-6'>
+        <label class="col-form-label" for="inputDefault">File type</label><br/>
+        <select name="file_type" id="file_type" class='form-control cil_san_regular_font'>
+            <option value="Mrc">Mrc</option>
+            <option value="Zip">Zip</option>
+        </select>
+    </div>
+    <div class='col-md-6'></div>
+</div>
+
+
+<div class="row">
+    <div class='col-md-12'>
+        <label class="col-form-label" for="inputDefault">File path</label>
+        <input id="alt_file_path" name="alt_file_path" style="width: 100%" type="text" value="" class="form-control cil_san_regular_font">
+    </div>
+    
+</div>
+
+
+<div class="row">
+    <div class='col-md-12'>
+        <label class="col-form-label" for="inputDefault">URL postfix</label>
+        <input id="alt_url_postfix" name="alt_url_postfix" style="width: 100%" type="text" value="" class="form-control cil_san_regular_font">
+    </div>
+    
+</div>
+
+
+
+<div class="row">
+    <div class='col-md-4'>
+        <label class="col-form-label" for="inputDefault">File size</label>
+        <input id="alt_file_size" name="alt_file_size" style="width: 100%" type="text" value="" class="form-control cil_san_regular_font">
+    </div>
+    <div class='col-md-8'></div>
+</div>
